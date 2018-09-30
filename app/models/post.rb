189fcc7ba39248993d83dc251b7151055b97d9b9
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-10-01 02:16:11 +0300

    Выполнено задание 4.1

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,13 @@ class Post < ApplicationRecord
   validates :title, uniqueness: true
   validates :body, length: { maximum: 2000, too_long: "TL:DR" }
 
+  # Задание №1 урока 4. Классовый метод, возвращающий список всех постов, написанных
+  # пользователями со статусов модератор. Проверка показа корректность генерации SQL:
+  # irb(main):035:0> y Post.from_moderators.pluck(:title)
+  # (1.2ms)  SELECT "posts"."title" FROM "posts"
+  #          INNER JOIN "users" ON "users"."id" = "posts"."user_id"
+  #          WHERE "users"."moderator" = $1  [["moderator", true]]
+  def self.from_moderators
+    joins(:user).where(users: { moderator: true })
+  end
 end
