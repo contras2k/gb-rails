96ddb56ad9be9cccb31343b48108d014950e490d
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-10-08 15:53:26 +0300

    Д/з 6, задание 3. Авторизация через Google OAuth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users
   get 'users/page/(:page(.:format))', to: 'users#index'
   resources :posts
